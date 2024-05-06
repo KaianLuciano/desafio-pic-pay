b6ef8bb4af97df3de01bef40e55bffb9a9298603
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-05 22:39:19 -0300

    feature(User): Update CNPJ Converter param for Document

--- src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cnpj/CNPJConverter.java
+++ src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cnpj/CNPJConverter.java
@@ -1,13 +1,14 @@
 package com.picpaychallenge.common.domain.model.valueobjects.cnpj;
 
+import com.picpaychallenge.common.domain.model.valueobjects.document.Document;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter
-public class CNPJConverter implements AttributeConverter<CNPJ, String> {
+public class CNPJConverter implements AttributeConverter<Document, String> {
 
     @Override
-    public String convertToDatabaseColumn(CNPJ attribute) {
+    public String convertToDatabaseColumn(Document attribute) {
         return attribute.getValue().replaceAll("[^0-9]", "");
     }
 
