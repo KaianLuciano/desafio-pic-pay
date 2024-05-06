4c2750e1730a8d6c812aab571cf90150f94458ad
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-06 18:25:00 -0300

    feature(User): Create Controller

---
+++ src/main/java/com/picpaychallenge/user/payload/UserController.java
@@ -0,0 +1,13 @@
+package com.picpaychallenge.user.payload;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("User Controller")
+@RequestMapping("${picpay.challenge.base.path}/${picpay.challenge.base.version}/clinic-units")
+@RequiredArgsConstructor
+@Tag(name = "User")
+public class UserController {
+}
