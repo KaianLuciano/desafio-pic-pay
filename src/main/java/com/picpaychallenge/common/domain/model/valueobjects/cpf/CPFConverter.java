0284503b726d45f07133b4586864c6f45556f6c9
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-05 22:38:14 -0300

    feature(User): Update CPFConverter param for Document

--- src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cpf/CPFConverter.java
+++ src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cpf/CPFConverter.java
@@ -1,16 +1,17 @@
 package com.picpaychallenge.common.domain.model.valueobjects.cpf;
 
+import com.picpaychallenge.common.domain.model.valueobjects.document.Document;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 @Converter
-public class CPFConverter implements AttributeConverter<CPF, String> {
+public class CPFConverter implements AttributeConverter<Document, String> {
 
     @Override
-    public String convertToDatabaseColumn(CPF cpfAttribute) {
-        return !ObjectUtils.isEmpty(cpfAttribute) && StringUtils.hasText(cpfAttribute.getValue()) ? cpfAttribute.getValue().replaceAll("\\D", "") : null;
+    public String convertToDatabaseColumn(Document attribute) {
+        return !ObjectUtils.isEmpty(attribute) && StringUtils.hasText(attribute.getValue()) ? attribute.getValue().replaceAll("\\D", "") : null;
     }
 
     @Override
