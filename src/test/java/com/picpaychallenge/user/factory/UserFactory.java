32c2866a0a5e6da32baacc116c442666e169b69c
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-11 20:52:05 -0300

    feature(User): Create Get userFormForPut and userFormForPost in UserFactory

--- src/test/java/com/picpaychallenge/user/factory/UserFactory.java
+++ src/test/java/com/picpaychallenge/user/factory/UserFactory.java
@@ -1,4 +1,28 @@
 package com.picpaychallenge.user.factory;
 
+import com.picpaychallenge.common.domain.model.valueobjects.cpf.CPF;
+import com.picpaychallenge.user.payload.TypeUser;
+import com.picpaychallenge.user.payload.UserDTO;
+import com.picpaychallenge.user.payload.UserForm;
+
 public class UserFactory {
+
+    public static UserForm getUserFormForPost() {
+        UserForm userForm = new UserForm();
+        userForm.setTypeUser(TypeUser.COMMON);
+        userForm.setDocument(new CPF("12345678901"));
+        userForm.setEmail("test@gmail.com");
+        userForm.setPassword("senhaTeste123@");
+        return userForm;
+    }
+
+    public static UserForm getUserFormForPut() {
+        UserForm userForm = new UserForm();
+        userForm.setTypeUser(TypeUser.COMMON);
+        userForm.setDocument(new CPF("987654321"));
+        userForm.setEmail("testupdate@gmail.com");
+        userForm.setPassword("senhaUpdateTeste123@");
+        return userForm;
+    }
+
 }
