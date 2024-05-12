861548d8c3bdbf72aa9bf523137f3cdf85208f0e
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-12 17:15:41 -0300

    feature(User): Implemented DocumentConverter

--- src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cnpj/CNPJConverter.java
+++ src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cnpj/CNPJConverter.java
@@ -1,11 +1,12 @@
 package com.picpaychallenge.common.domain.model.valueobjects.cnpj;
 
 import com.picpaychallenge.common.domain.model.valueobjects.document.Document;
+import com.picpaychallenge.common.domain.model.valueobjects.document.DocumentConverter;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter
-public class CNPJConverter implements AttributeConverter<Document, String> {
+public class CNPJConverter implements DocumentConverter {
 
     @Override
     public String convertToDatabaseColumn(Document attribute) {
--- src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cpf/CPFConverter.java
+++ src/main/java/com/picpaychallenge/common/domain/model/valueobjects/cpf/CPFConverter.java
@@ -1,13 +1,14 @@
 package com.picpaychallenge.common.domain.model.valueobjects.cpf;
 
 import com.picpaychallenge.common.domain.model.valueobjects.document.Document;
+import com.picpaychallenge.common.domain.model.valueobjects.document.DocumentConverter;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 @Converter
-public class CPFConverter implements AttributeConverter<Document, String> {
+public class CPFConverter implements DocumentConverter {
 
     @Override
     public String convertToDatabaseColumn(Document attribute) {
