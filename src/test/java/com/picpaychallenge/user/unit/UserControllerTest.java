a1770bf6603861a317d32e1324a85d7f34a87ad2
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-05-12 17:20:02 -0300

    feature(User): Config UserControllerTest

--- src/test/java/com/picpaychallenge/user/unit/UserControllerTest.java
+++ src/test/java/com/picpaychallenge/user/unit/UserControllerTest.java
@@ -1,4 +1,27 @@
 package com.picpaychallenge.user.unit;
 
+import com.picpaychallenge.user.UserController;
+import com.picpaychallenge.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+    @MockBean
+    private UserService userService;
+    private MockMvc mvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @BeforeEach
+    void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 }
